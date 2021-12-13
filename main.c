8a23b10ac7ff92ea48590e540c21bd6d17646401
Karol Borecki <karol.borecki@onet.eu>
2021-12-13 15:05:16 +0100

    Initzialized main.c, Added gitignore - ignore folder and DS_Store for MacOS users

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+*
+* Created by Karol Borecki on 13.12.21 <karol.boreck@gmail.com>
+*
+*/
+
+#include <stdio.h>
+
+int main(int argc, char** argv){
+  printf("%s: Hello!", argv[0]);
+
+  return 0;
+}
